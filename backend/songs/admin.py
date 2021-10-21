210880e97dd32b9868ed16abb0a3b0bf7065c8f2
jermin2 <jermin2@gmail.com>
2021-10-21 18:35:20 +1300

    setup backed with basic song and api

--- backend/songs/admin.py
+++ backend/songs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Song
 
 # Register your models here.
+class SongAdmin(admin.ModelAdmin):
+    list = ('title', 'text')
+
+admin.site.register(Song, SongAdmin)
