8c8ab6f143a21fb29d153864f09d624aab516245
jermin2 <jermin2@gmail.com>
2021-10-22 15:57:20 +1300

    add books into the backend

--- backend/songs/admin.py
+++ backend/songs/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Song
+from .models import *
 
 # Register your models here.
 class SongAdmin(admin.ModelAdmin):
     list = ('title', 'text')
 
 admin.site.register(Song, SongAdmin)
+admin.site.register(Book)
+admin.site.register(BookSongs)
--- backend/songs/models.py
+++ backend/songs/models.py
@@ -7,3 +7,17 @@ class Song(models.Model):
 
     def _str_(self):
         return self.title
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    year = models.IntegerField()
+    songs = models.ManyToManyField(Song, through="BookSongs")
+
+    def _str_(self):
+        return self.title
+
+class BookSongs(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    index = models.IntegerField()
+    variant = models.TextField(blank=True)
